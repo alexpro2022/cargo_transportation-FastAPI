51488b8b7630feb5df17043b8515d45c40c0d33b
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-05-25 01:40:57 +0300

    Made a load decorator

--- app/core/init_db.py
+++ app/core/init_db.py
@@ -7,30 +7,33 @@ from app.models.location import Location
 from app.models.car import Car
 
 
-async def load(data: list):
-    data_name = f'={data[0].__class__.__name__.lower()}s=' if data else '=None='
-    session = AsyncSessionLocal()
-    session.add_all(data)
-    try:
-        print(f'Loading data {data_name} ...')
-        await session.commit()
-    except IntegrityError:
-        print(f'Data {data_name} has already been loaded ... exiting')
-        await session.rollback()
-    else:
-        print(f'Data {data_name} has successfully been loaded')
+def load(func):
+    async def wrapper(*args, **kwargs):
+        data = func(*args, **kwargs)
+        data_name = f'={data[0].__class__.__name__.lower()}s=' if data else '=None='
+        session = AsyncSessionLocal()
+        session.add_all(data)
+        try:
+            print(f'Loading data {data_name} ...')
+            await session.commit()
+        except IntegrityError:
+            print(f'Data {data_name} has already been loaded ... exiting')
+            await session.rollback()
+        else:
+            print(f'Data {data_name} has successfully been loaded')
+    return wrapper
 
 
-async def load_locations():
+@load
+def load_locations():
     columns = [column.key for column in Location.__table__.columns]
     with open(
         'app/core/data/uszips.csv', encoding='utf-8', newline=''
     ) as csv_file:
         csvreader = csv.DictReader(csv_file, quotechar='"')
-        locations = [Location(**{key: value for key, value in row.items() if key in columns}) for row in csvreader]
-        await load(locations)
+        return [Location(**{key: value for key, value in row.items() if key in columns}) for row in csvreader]
 
 
-async def load_cars():
-    cars = []
-    await load(cars)
+@load
+def load_cars():
+    return []
--- app/main.py
+++ app/main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI
 
 from app.api.routers import main_router
 from app.core.config import settings
-from app.core.init_db import load_locations
-# from app.core.init_db import fill_data
+from app.core.init_db import load_cars, load_locations
 
 app = FastAPI(
     title=settings.app_title,
@@ -15,5 +14,5 @@ app.include_router(main_router)
 
 @app.on_event('startup')
 async def startup():
+    await load_cars()
     await load_locations()
-    # await check_db_connection()
